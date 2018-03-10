5d1b6ea9ca106327d569783fc26a39aeded3cfaa
Afshin <a.hamedbarghi@gmail.com>
2018-03-10 15:13:26 +0100

    Sample survey data loaded and number of questions are shown

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Survey from './Survey';
 
 const App = () => {
   return (
     <div>
       <h2>Survey 2018</h2>
-      
+      <Survey />
     </div>
   )
 }
---
+++ src/components/Survey.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import axios from 'axios'
+import Question from './Question'
+
+
+export default class Survey extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            survey: {},
+        }
+    }
+
+    async componentDidMount() {
+        const data = await axios.get('/data/quiz.json');
+        this.setState({ survey: data });
+    }
+
+
+    render() {
+        const {
+      survey
+    } = this.state
+
+        let numberOfQuestions = survey.data ? survey.data.questions.length : 0
+
+        return (
+            <div>
+                <h1>{survey.title}</h1>
+                <div>
+                    {/* Progress bar */}
+                    <h2>Question {1} of {numberOfQuestions}</h2>
+                </div>
+            </div>
+        )
+    }
+}
