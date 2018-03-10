ad6c22289545c73a0fdbcb8f044b32c47d9d25f8
Afshin <a.hamedbarghi@gmail.com>
2018-03-10 18:29:58 +0100

    Basic Survey functionality and summary page implemented

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,13 +2,26 @@ import React from 'react'
 
 const Question = ({
   question,
+    index,
+    onAnswerSelected,
+    onSubmit
 }) => {
-  return (
-    <div>
-      <h3>{question.question}</h3>
-    </div>
-  )
+    return (
+        <div>
+            <h3>{question.question}</h3>
+            <ol>
+                {question.answers.map((answer, i) =>
+                    <li key={`${index}-${i}`}>
+                        <input type="radio" name={`question_${index}`} id={`question_${index}_answer_${i}`}
+                            defaultChecked={false} value={i} onChange={onAnswerSelected} />
+                        {' '}
+                        <label htmlFor={`question_${index}_answer_${i}`}>{answer.label}</label>
+                    </li>
+                )}
+            </ol>
+            <button onClick={onSubmit}>Submit</button>
+        </div>
+    )
 }
 
-
-export default Question
+export default Question;
--- src/components/Survey.js
+++ src/components/Survey.js
@@ -9,6 +9,8 @@ export default class Survey extends React.Component {
         super(props)
         this.state = {
             survey: {},
+            index: 0,
+            answers: []
         }
     }
 
@@ -17,20 +19,62 @@ export default class Survey extends React.Component {
         this.setState({ survey: data });
     }
 
-    render() {
-        const {
-      survey
-    } = this.state
 
-        let numberOfQuestions = survey.data ? survey.data.questions.length : 0
+    handleSubmit() {
+        this.state.index < this.state.survey.data.questions.length ?
+            this.setState({ 'index': this.state.index + 1 }) : ''
+    }
+
+    handleAnswerSelected(event) {
+        let list = [...this.state.answers.slice(0, this.state.index),
+        parseInt(event.target.value),
+        ...this.state.answers.slice(this.state.index + 1)];
+
+        this.setState({ 'answers': list })
+    }
+
+    summaryCreator() {
+
+        return this.state.answers.map((answer, i) => {
+            return <li key={i}>
+                {this.state.survey.data.questions[i].answers[answer].label}
+            </li>
+        });
+    }
+
+    render() {
+
+        const { survey, index, answers } = this.state;
+        const numberOfQuestions = survey.data ? survey.data.questions.length : 0;
+
+        let completed = (survey.data && (index === survey.data.questions.length)) ? true : false;
+        let summary;
+
         return (
             <div>
                 <h1>{survey.title}</h1>
-                <div>
-                    {/* Progress bar */}
-                    <h2>Question {1} of {numberOfQuestions}</h2>
-                    {survey.data ? <Question question={survey.data.questions[0]} />: ''}
-                </div>
+                {completed ?
+
+                    <div>
+                        {/* Summary Page */}
+                        <p>Congratulation, you finished the survey</p>
+                        Summary {this.summaryCreator()}
+                    </div>
+                    :
+                    <div>
+                        {/* Progress bar */}
+                        <h2>Question {index + 1} of {numberOfQuestions}</h2>
+                        {survey.data && index < numberOfQuestions ?
+                            <Question
+                                question={survey.data.questions[index]}
+                                index={index}
+                                onAnswerSelected={(event) => this.handleAnswerSelected(event)}
+                                onSubmit={() => this.handleSubmit()}
+                            />
+
+                            : 'completed'}
+                    </div>
+                }
             </div>
         )
     }
